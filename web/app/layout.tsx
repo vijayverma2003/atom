ab3febfac599963cd113de608451b7628f1b7312
Vijay <vijayvermakvp@gmail.com>
2025-08-29 16:55:09 +0530

    Add web app

---
+++ web/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
+import "./globals.css";
+
+const lexend = Lexend({
+  variable: "--font-lexend",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ATOM",
+  description: "Share images with people all over the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${lexend.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
